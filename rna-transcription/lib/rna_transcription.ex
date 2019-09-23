027844b14e0b151644ee19587194bf344d071d84
lucassouto <lucasinfologos@gmail.com>
2019-09-23 01:16:00 -0300

    Adicionar desafio RNATranscription

---
+++ rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,20 @@
+defmodule RnaTranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    rna_map = %{
+      ?G => ?C,
+      ?C => ?G,
+      ?T => ?A,
+      ?A => ?U,
+    }
+    Enum.map(dna, fn(x) -> rna_map[x] end)
+  end
+end
