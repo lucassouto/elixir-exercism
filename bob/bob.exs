23a7ba28392070a015caa5497d6087bcc1869723
lucassouto <lucasinfologos@gmail.com>
2019-09-24 00:22:06 -0300

    Adicionar desafio Bob

---
+++ bob/bob.exs
@@ -0,0 +1,15 @@
+defmodule Bob do
+  def hey(input) do
+    cond do
+      empty?(input) -> "Fine. Be that way!"
+      upcase?(input) && String.ends_with?(input, "?") -> "Calm down, I know what I'm doing!"
+      String.ends_with?(input, "?") -> "Sure."
+      upcase?(input) -> "Whoa, chill out!"
+      true -> "Whatever."
+    end
+  end
+
+  defp empty?(phrase), do: String.trim(phrase) == ""
+  defp upcase?(phrase), do: String.upcase(phrase) == phrase && String.downcase(phrase) != phrase
+
+end
