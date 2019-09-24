aeb2075c25cb3a56a65141d601d6332928432c58
lucassouto <lucasinfologos@gmail.com>
2019-09-24 00:30:50 -0300

    Atualizar desafio Bob de nova versão do exercism

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,4 +1,6 @@
 defmodule Bob do
+
+  @spec hey(String.t) :: String.t
   def hey(input) do
     cond do
       empty?(input) -> "Fine. Be that way!"
@@ -11,5 +13,4 @@ defmodule Bob do
 
   defp empty?(phrase), do: String.trim(phrase) == ""
   defp upcase?(phrase), do: String.upcase(phrase) == phrase && String.downcase(phrase) != phrase
-
 end
