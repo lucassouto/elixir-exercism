b9c30cb209ba26a26645f051c9e73a8ee47c6682
Lucas Souto <lucasinfologos@gmail.com>
2019-09-27 09:15:47 -0300

    Remover replace desnecessário

--- word-count/lib/words.ex
+++ word-count/lib/words.ex
@@ -8,8 +8,7 @@ defmodule Words do
   def count(sentence) do
     sentence
     |> String.downcase
-    |> String.replace(~r/([^\w\-]|_)+/u, " ")
-    |> String.split([" ", ","], trim: true)
+    |> String.split(~r/([^\w\-]|_)+/u, trim: true)
     |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
   end
 end
